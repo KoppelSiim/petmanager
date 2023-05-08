a610f5a55df4b8133d76cdbc7045a9871d2fc97e
Siim Koppel <siimk999@gmail.com>
2023-05-08 18:06:07 +0300

    Minor changes

--- src/main/java/com/wisercat/petmanagement/MainController.java
+++ src/main/java/com/wisercat/petmanagement/MainController.java
@@ -3,6 +3,7 @@ package com.wisercat.petmanagement;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.wisercat.petmanagement.model.Pets;
 import com.wisercat.petmanagement.service.PetService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = "/pet")
 public class MainController {
 
@@ -29,26 +31,22 @@ public class MainController {
         List<Pets> pets = petService.findAllPets();
         return new ResponseEntity<>(pets, HttpStatus.OK);
     }
-
     @GetMapping("/find/{id}")
     public ResponseEntity<Pets> getPetById(@PathVariable("id") Long id) {
         Pets pet = petService.findPetById(id);
         return new ResponseEntity<>(pet, HttpStatus.OK);
     }
-
     @PostMapping("/add")
     public ResponseEntity<Pets> addPet(@RequestBody Pets pet) {
         Pets newpet = petService.addPet(pet);
         return new ResponseEntity<Pets>(newpet, HttpStatus.CREATED);
     }
-
     @PutMapping("/update/{id}")
     public ResponseEntity<Pets> updatePet(@PathVariable("id") Long id, @RequestBody Pets pet) {
         Pets pettoupdate = petService.findPetById(id);
         Pets updatedpet = petService.updatePet(pettoupdate, pet);
         return new ResponseEntity<Pets>(updatedpet, HttpStatus.OK);
     }
-
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deletePet(@PathVariable("id") Long id) {
         petService.deletePet(id);
